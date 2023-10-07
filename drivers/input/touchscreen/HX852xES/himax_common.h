b8aa1197e3918462c37ee1641c8c0fd2aa432440
Saalim Quadri <danascape@gmail.com>
2023-10-07 10:38:34 +0530

    drivers: input: HX852xES: Update wakeup_source API
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- drivers/input/touchscreen/HX852xES/himax_common.h
+++ drivers/input/touchscreen/HX852xES/himax_common.h
@@ -317,7 +317,7 @@ struct himax_ts_data
 #ifdef HX_SMART_WAKEUP
     uint8_t SMWP_enable;
     uint8_t gesture_cust_en[16];
-    struct wakeup_source ts_SMWP_wake_lock;
+    struct wakeup_source *ts_SMWP_wake_lock;
 #endif
 
 #ifdef HX_HIGH_SENSE
